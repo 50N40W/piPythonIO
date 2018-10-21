df7d7aaf1ffdb088962345474807e7489df796f7
50N40W <bishop038@hotmail.com>
2018-10-21 11:18:34 -0400

    changes for clarity and a little more correctness

--- blink2_noDelay.py
+++ blink2_noDelay.py
@@ -1,15 +1,13 @@
-import RPi.GPIO as GPIO
-import noDelayIO as mio
 import time
+import noDelayIO as mio
 millis = int(round(time.time() * 1000))
 #startTime = millis
 LedPin = 11  # LED No. 1
 Led2Pin = 7  # LED No. 2
 Led3Pin = 19 # LED No. 3
+blinkPeriod = 5000
 
 sw1Pin = 40  # Switch No. 1
-
-blinkPeriod = 5000
 swPeriod = 20
 dbTime = 2000
 
@@ -21,42 +19,34 @@ Lamp3 = mio.Lampy(Led3Pin,int(blinkPeriod*4/7))
 
 Switch1 = mio.Switchy(sw1Pin,swPeriod,dbTime)
 
-def setup():
-    #*** Uncomment out these lines for use on RPi ***
-    # Use Physical Pin Number rather than silkscreen
-    GPIO.setmode(GPIO.BOARD)
-    print("in setup")
-    GPIO.setup(LedPin, GPIO.OUT)   
-    GPIO.output(LedPin, GPIO.HIGH)
-    
-    GPIO.setup(Led2Pin, GPIO.OUT)   
-    GPIO.output(Led2Pin, GPIO.HIGH)
-
-    GPIO.setup(sw1Pin, GPIO.IN, pull_up_down=GPIO.PUD_UP)
-    #GPIO.setup(18, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
-
 def loop():
     while True:
+        # we could invoke time in each objects
+        # method.   But that means 3 sys calls
+        # per loop. This way takes only 1 call.
         millis = int(round(time.time() * 1000))
         Lamp1.time = millis
         Lamp2.time = millis
-        #Lamp3.time = millis
+        Lamp3.time = millis
+        Switch1.time = millis
+        
         Lamp1.LampCheck()
         Lamp2.LampCheck()
-        #Lamp3.LampCheck()
+        Lamp3.LampCheck()
 
-        #until we hook it up to real IO, use the pretend
-        # lamp1 state to simulate a switch input.
-        #Switch1.rawState = Lamp1.lampState
-        Switch1.time = millis
         Switch1.debounce()
+        #if sw1 is "hot" override lamp1 to ON
+        if Switch1.switchState == True:
+            Lamp1.override = True
+            Lamp1.overrideValue = "high"
 
 def destroy():
     GPIO.output(LedPin, GPIO.LOW)     # led off
+    GPIO.output(Led2Pin, GPIO.LOW)     # led off
+    GPIO.output(Led3Pin, GPIO.LOW)     # led off
     GPIO.cleanup()                     # Release resource
 
 if __name__ == '__main__':     # Program start from here
-    setup()
     try:
         loop()
     except KeyboardInterrupt:
